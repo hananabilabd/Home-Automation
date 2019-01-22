6d5850d7743433b62b277b1f6cee2e3011f545d6
Hanna Nabil <hananabil@live.com>
2019-01-22 12:20:20 +0200

    added uart stm template

---
+++ STM32f103C8_UART/User/main.c
@@ -0,0 +1,25 @@
+#include "stm32f10x.h"
+#include "stm32f10x_rcc.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_usart.h"
+#include "delay1.h"
+
+
+
+
+uint8_t receivedByte;
+	
+int main(void)
+{
+	
+	
+	while (1)
+	{
+		// Write your code
+	
+		
+		
+	}
+}
+
+
