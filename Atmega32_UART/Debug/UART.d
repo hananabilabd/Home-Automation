134944603a7eab1e7d83cd7e5dc0f67f0f8830c5
Hanna Nabil <hananabil@live.com>
2019-01-23 17:55:50 +0200

    last commit before assaf

---
+++ Atmega32_UART/Debug/UART.d
@@ -0,0 +1,12 @@
+UART.o UART.o: ../UART.c ../typedef.h ../IO.h ../macros.h ../DIO.h \
+  ../UART.h
+
+../typedef.h:
+
+../IO.h:
+
+../macros.h:
+
+../DIO.h:
+
+../UART.h:
---
+++ Atmega32_UART/main.c
@@ -0,0 +1,44 @@
+/*
+ * main.c
+ *
+ *  Created on: Feb 24, 2018
+ *      Author: Hanna Nabil
+ */
+
+#include "typedef.h"
+#include "IO.h"
+#include "macros.h"
+#define F_CPU 8000000UL
+#include <util/delay.h>
+#include "DIO.h"
+#include "UART.h"
+// This assignment is to make a LDR controlling a Led in an inversely manner
+int main(void)
+{
+	u8 arr []="Hannon";
+	u8 a []="han";
+
+	int i =0;
+	UART_INIT();
+	//DIO_SetPinDirection('D',7,1);
+	//DIO_SetPinValue('D',7,1);
+	u8 Data;
+	while (1){
+		//Data = UART_ReceiveData();
+		//Data = UART_Check_ReceivedString(a);
+
+
+			UART_TransmitData(0);
+			UART_TransmitData(1);
+
+
+
+		/*
+		 if (Data==1){
+			DIO_SetPinValue('D',7,1);
+		}*/
+
+	}
+	return 0;
+}
+
